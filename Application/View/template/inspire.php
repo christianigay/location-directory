9a3ec67be8910db6a9cbf333f86f3dd22164244c
Christian <christian.igay@gmail.com>
2025-10-26 16:51:40 +0800

    quote fix

--- Application/View/template/inspire.php
+++ Application/View/template/inspire.php
@@ -1,43 +1,64 @@
 <html>
-  <head>
-    <title>Inspired-Conf26-web</title>
-    <meta charset="utf-8" />
-    <?php foreach ($this->cssFiles as $file): ?>
-      <link rel="stylesheet" type="text/css" href="<?php echo $file; ?>">
-    <?php endforeach; ?>
-  </head>
 
-  <body>
-    <div class="container desktop-only">
+<head>
+  <title>Inspired-Conf26-web</title>
+  <meta charset="utf-8" />
+  <?php foreach ($this->cssFiles as $file): ?>
+    <link rel="stylesheet" type="text/css" href="<?php echo $file; ?>">
+  <?php endforeach; ?>
+</head>
 
-      <!-- === HERO SECTION === -->
-      <div class="section hero-main">
-        <div class="hero-overlay">
-          <button>REGISTER NOW</button>
-          <div>SATURDAY, APRIL 25</div>
-        </div>
+<body>
+  <div class="container desktop-only">
 
-        <div class="title-container">
-          <video autoplay muted loop playsinline>
-            <source src="videos/Inspired_desktop_output.mp4" type="video/mp4"> 
-            Your browser does not support the video tag.
-          </video>
-
-          <div class="main-title"></div>
-
-          <div class="conference-title">
-            THE INSPIRED CONFERENCE 2026
-          </div>
-        </div>
+    <!-- === HERO SECTION === -->
+    <div class="section hero-main">
+      <div class="hero-overlay">
+        <button>REGISTER NOW</button>
+        <div>SATURDAY, APRIL 25</div>
       </div>
 
-      <!-- === REGISTER SECTION === -->
-      <div class="section register">
-        <div class="register-text">
-          <div>REGISTER NOW</div>
+      <div class="title-container">
+        <video autoplay muted loop playsinline>
+          <source src="videos/Inspired_desktop_output.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+
+        <div class="main-title"></div>
+
+        <div class="conference-title">
+          THE INSPIRED CONFERENCE 2026
         </div>
       </div>
-
     </div>
-  </body>
+
+    <!-- === REGISTER SECTION === -->
+    <div class="section register">
+      <div class="register-text">
+        <div>REGISTER NOW</div>
+      </div>
+    </div>
+
+    <div class="quote-row">
+      <div class="quote-part start">
+      </div>
+      <div class="quote-part middle">
+        <div>Here's another way to put it: You're here to be light, bringing out</div>
+        <div>the God-colors in the world. God is not a secret be kept. We're</div>
+        <div>going public with this, as public as a acity on a hill. If I make you</div>
+        <div>light-bearers, you don't think I'm going to hide you under a bucket,</div>
+        <div>do you? I'm putting you on a light stand. Now that I've put you there</div>
+        <div>on a hilltop, on a light stand&mdash;shine! Keep open house; be generous</div>
+        <div>with your lives. By opening up to others, you'll prompt people to</div>
+        <div>open up with God, this generous Father in heaven.</div>
+        <div class="quote-author">Matthew 5:14-16 MSG</div>
+        <!-- <img src="images/quote-middle-content.jpg" alt=""> -->
+      </div>
+      <div class="quote-part end">
+      </div>
+    </div>
+
+  </div>
+</body>
+
 </html>
