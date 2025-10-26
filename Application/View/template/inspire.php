72a197b8aff758c71780a5e07afd65a3aaed30ec
Christian <christian.igay@gmail.com>
2025-10-26 19:22:44 +0800

    fix

--- Application/View/template/inspire.php
+++ Application/View/template/inspire.php
@@ -39,26 +39,27 @@
       </div>
     </div>
 
-    <!-- Quote Section -->
-    <div class="three-column">
-      <div class="col col-middle">
-        <div class="quote-main-center">
-          <div>Here's another way to put it: You're here to be light, bringing out</div>
-          <div>the God-colors in the world. God is not a secret be kept. We're</div>
-          <div>going public with this, as public as a city on a hill. If I make you</div>
-          <div>light-bearers, you don't think I'm going to hide you under a bucket,</div>
-          <div>do you? I'm putting you on a light stand. Now that I've put you there</div>
-          <div>on a hilltop, on a light stand—shine! Keep open house; be generous</div>
-          <div>with your lives. By opening up to others, you'll prompt people to</div>
-          <div>open up with God, this generous Father in heaven.</div>
-          <div class="quote-author">Matthew 5:14–16 MSG</div>
+    <div>
+      <div class="three-column">
+        <div class="col col-middle">
+          <div class="quote-main-center">
+            <div>Here's another way to put it: You're here to be light, bringing out</div>
+            <div>the God-colors in the world. God is not a secret be kept. We're</div>
+            <div>going public with this, as public as a city on a hill. If I make you</div>
+            <div>light-bearers, you don't think I'm going to hide you under a bucket,</div>
+            <div>do you? I'm putting you on a light stand. Now that I've put you there</div>
+            <div>on a hilltop, on a light stand—shine! Keep open house; be generous</div>
+            <div>with your lives. By opening up to others, you'll prompt people to</div>
+            <div>open up with God, this generous Father in heaven.</div>
+            <div class="quote-author">Matthew 5:14–16 MSG</div>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div class="section theme-section">
-      <img src="assets/css/images/index_07.jpg" alt="Theme Image" />
-      <img src="assets/css/images/main-content-image.png" alt="Theme Image" />
+      <div class="section theme-section">
+        <img src="assets/css/images/index_07.jpg" alt="Theme Image" />
+        <img src="assets/css/images/main-content-image.png" alt="Theme Image" />
+      </div>
     </div>
 
     <div class="section theme-section2">
