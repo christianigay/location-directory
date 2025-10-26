0f39d19c1563d76db83d317c09e4fd54da9ee22a
Christian <christian.igay@gmail.com>
2025-10-26 17:59:07 +0800

    midle fix

---
+++ Application/View/template/inspire copy 2.php
@@ -0,0 +1,85 @@
+<html>
+
+<head>
+  <title>Inspired-Conf26-web</title>
+  <meta charset="utf-8" />
+  <?php foreach ($this->cssFiles as $file): ?>
+    <link rel="stylesheet" type="text/css" href="<?php echo $file; ?>">
+  <?php endforeach; ?>
+</head>
+
+<body>
+  <div class="container desktop-only">
+
+    <!-- === HERO SECTION === -->
+    <div class="section hero-main">
+      <div class="hero-overlay">
+        <button>REGISTER NOW</button>
+        <div>SATURDAY, APRIL 25</div>
+      </div>
+
+      <div class="title-container">
+        <video autoplay muted loop playsinline>
+          <source src="videos/Inspired_desktop_output.mp4" type="video/mp4">
+          Your browser does not support the video tag.
+        </video>
+
+        <div class="main-title"></div>
+
+        <div class="conference-title">
+          THE INSPIRED CONFERENCE 2026
+        </div>
+      </div>
+    </div>
+
+    <!-- === REGISTER SECTION === -->
+    <div class="section register">
+      <div class="register-text">
+        <div>REGISTER NOW</div>
+      </div>
+    </div>
+
+    <div class="quote-row">
+      <div class="quote-part start">
+      </div>
+      <div class="quote-part middle">
+        <div>Here's another way to put it: You're here to be light, bringing out</div>
+        <div>the God-colors in the world. God is not a secret be kept. We're</div>
+        <div>going public with this, as public as a acity on a hill. If I make you</div>
+        <div>light-bearers, you don't think I'm going to hide you under a bucket,</div>
+        <div>do you? I'm putting you on a light stand. Now that I've put you there</div>
+        <div>on a hilltop, on a light stand&mdash;shine! Keep open house; be generous</div>
+        <div>with your lives. By opening up to others, you'll prompt people to</div>
+        <div>open up with God, this generous Father in heaven.</div>
+        <div class="quote-author">Matthew 5:14-16 MSG</div>
+        <!-- <img src="images/quote-middle-content.jpg" alt=""> -->
+      </div>
+      <div class="quote-part end">
+      </div>
+    </div>
+
+    <div class="theme-title">
+      <div class="image-wrapper">
+        <img src="assets/css/images/main-content-image.png" alt="Main content image">
+      </div>
+    </div>
+
+    <div class="gallery-section">
+      <div class="quote-content">
+        <div class="quote-second-text"><span class="vibrant">Vibrant</span> <span class="adj">(adj.)</span></div>
+        <div class="quote-second-meaning">
+          <div>A SPIRIT ALIVE WITH COLOR AND ENERGY;</div>
+          <div>A PERSONALITY THAT RADIATES WARMTH, ENTHUSIASM,</div>
+          <div>AND LIFE. TO BE VIBRANT IS TO PULSE WITH</div>
+          <div>AUTHENTICITY&mdash;TO LIGHT UP ROOMS, LIFT OTHERS, AND</div>
+          <div>MAKE ORDINARY MOMENTS FEEL EXTRAORDINARY</div>
+        </div>
+      </div>
+    </div>
+
+    <div>test</div>
+
+  </div>
+</body>
+
+</html>
