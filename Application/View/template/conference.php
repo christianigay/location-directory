5644adfbc83e9519564c668cfadd81d7950c9d11
Christian <christian.igay@gmail.com>
2025-10-26 12:50:06 +0800

    fix title

--- Application/View/template/conference.php
+++ Application/View/template/conference.php
@@ -38,62 +38,67 @@
 			</div>
 		</div>
 
-		<div class="row quote-row">
-			<div class="quote-main-column"></div>
+		<div>
 
-			<div class="quote-main-center">
-				<div>Here's another way to put it: You're here to be light, bringing out</div>
-				<div>the God-colors in the world. God is not a secret be kept. We're</div>
-				<div>going public with this, as public as a acity on a hill. If I make you</div>
-				<div>light-bearers, you don't think I'm going to hide you under a bucket,</div>
-				<div>do you? I'm putting you on a light stand. Now that I've put you there</div>
-				<div>on a hilltop, on a light stand&mdash;shine! Keep open house; be generous</div>
-				<div>with your lives. By opening up to others, you'll prompt people to</div>
-				<div>open up with God, this generous Father in heaven.</div>
-				<div class="quote-author">Matthew 5:14-16 MSG</div>
+
+
+			<div class="row quote-row">
+				<div class="quote-main-column"></div>
+
+				<div class="quote-main-center">
+					<div>Here's another way to put it: You're here to be light, bringing out</div>
+					<div>the God-colors in the world. God is not a secret be kept. We're</div>
+					<div>going public with this, as public as a acity on a hill. If I make you</div>
+					<div>light-bearers, you don't think I'm going to hide you under a bucket,</div>
+					<div>do you? I'm putting you on a light stand. Now that I've put you there</div>
+					<div>on a hilltop, on a light stand&mdash;shine! Keep open house; be generous</div>
+					<div>with your lives. By opening up to others, you'll prompt people to</div>
+					<div>open up with God, this generous Father in heaven.</div>
+					<div class="quote-author">Matthew 5:14-16 MSG</div>
+				</div>
+				<div class="quote-main-end"></div>
 			</div>
-			<div class="quote-main-end"></div>
-		</div>
-		<!-- Row 6 -->
-		<div class="row">
-			<div style="flex: 0 0 100%;">
-				<img src="assets/css/images/theme-title.jpg" alt="">
-			</div>
-		</div>
-		<!-- Row 7 -->
-		<div class="row">
-			<div style="flex: 0 0 6.72%;">
-				<img src="assets/css/images/inex_13.jpg" alt="">
-			</div>
-			<div style="flex: 0 0 7.76%;">
-				<img src="assets/css/images/inex_14.jpg" alt="">
-			</div>
-			<div class="quote-second">
-				<div>
-					<div class="quote-second-text"><span class="vibrant">Vibrant</span> <span class="adj">(adj.)</span></div>
-					<div class="quote-second-meaning">
-						<div>A SPIRIT ALIVE WITH COLOR AND ENERGY;</div>
-						<div>A PERSONALITY THAT RADIATES WARMTH, ENTHUSIASM,</div>
-						<div>AND LIFE. TO BE VIBRANT IS TO PULSE WITH</div>
-						<div>AUTHENTICITY&mdash;TO LIGHT UP ROOMS, LIFT OTHERS, AND</div>
-						<div>MAKE ORDINARY MOMENTS FEEL EXTRAORDINARY</div>
-					</div>
+			<!-- Row 6 -->
+			<div class="row">
+				<div class="theme-title" style="flex: 0 0 100%;">
+					<img src="assets/css/images/theme-title-main-content.jpg" alt="">
 				</div>
 			</div>
-			<div style="flex: 0 0 7.50%;">
-				<img src="assets/css/images/inex_16.jpg" alt="">
-			</div>
-			<div style="flex: 0 0 6.92%;">
-				<img src="assets/css/images/inex_17.jpg" alt="">
-			</div>
-			<div style="flex: 0 0 6.98%;">
-				<img src="assets/css/images/inex_18.jpg" alt="">
-			</div>
-			<div style="flex: 0 0 7.08%;">
-				<img src="assets/css/images/inex_19.jpg" alt="">
-			</div>
-			<div style="flex: 0 0 15.42%;">
-				<img src="assets/css/images/inex_20.jpg" alt="">
+			<!-- Row 7 -->
+			<div class="row">
+				<div style="flex: 0 0 6.72%;">
+					<img src="assets/css/images/inex_13.jpg" alt="">
+				</div>
+				<div style="flex: 0 0 7.76%;">
+					<img src="assets/css/images/inex_14.jpg" alt="">
+				</div>
+				<div class="quote-second">
+					<div>
+						<div class="quote-second-text"><span class="vibrant">Vibrant</span> <span class="adj">(adj.)</span></div>
+						<div class="quote-second-meaning">
+							<div>A SPIRIT ALIVE WITH COLOR AND ENERGY;</div>
+							<div>A PERSONALITY THAT RADIATES WARMTH, ENTHUSIASM,</div>
+							<div>AND LIFE. TO BE VIBRANT IS TO PULSE WITH</div>
+							<div>AUTHENTICITY&mdash;TO LIGHT UP ROOMS, LIFT OTHERS, AND</div>
+							<div>MAKE ORDINARY MOMENTS FEEL EXTRAORDINARY</div>
+						</div>
+					</div>
+				</div>
+				<div style="flex: 0 0 7.50%;">
+					<img src="assets/css/images/inex_16.jpg" alt="">
+				</div>
+				<div style="flex: 0 0 6.92%;">
+					<img src="assets/css/images/inex_17.jpg" alt="">
+				</div>
+				<div style="flex: 0 0 6.98%;">
+					<img src="assets/css/images/inex_18.jpg" alt="">
+				</div>
+				<div style="flex: 0 0 7.08%;">
+					<img src="assets/css/images/inex_19.jpg" alt="">
+				</div>
+				<div style="flex: 0 0 15.42%;">
+					<img src="assets/css/images/inex_20.jpg" alt="">
+				</div>
 			</div>
 		</div>
 		<!-- Row 8 -->
